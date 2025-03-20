92979a08252f4fefcb1ed3d114763b0531153111
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2025-03-20 09:58:43 -0400

    Benchmark 2 version of the site

---
+++ app/public/wp-content/plugins/publishpress-checklists/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'publishpress/publishpress-checklists',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0f6970a2f7cccbe753976af3c70534c3728401ca',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'publishpress/publishpress-checklists' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0f6970a2f7cccbe753976af3c70534c3728401ca',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
